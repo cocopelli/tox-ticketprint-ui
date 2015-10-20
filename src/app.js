4bf475883c2c3ad9658e3b41ee8037ebea019c46
Enrico Hoffmann <dasrick@gmail.com>
2015-10-20 19:47:23 +0200

    add initial app.js, webpack and commands

---
+++ src/app.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+ * @ngInject
+ */
+var angular = require('angular');
+
+var requires = [];
+
+angular.module('tox-ticketprint-ui-app', requires);
+
+angular.bootstrap(document, ['tox-ticketprint-ui-app']);
