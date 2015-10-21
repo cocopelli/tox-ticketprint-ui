992662e71ea04749fd37294c0db529774f71711a
Enrico Hoffmann <dasrick@gmail.com>
2015-10-21 07:15:36 +0200

    split main template into three (app, form and preview)

--- src/components/core/controller/CoreController.js
+++ src/components/core/controller/CoreController.js
@@ -5,4 +5,12 @@
 module.exports = function () {
   var vm = this;
   vm.appversion = process.env.appversion;
+  vm.formData = {};
+  vm.printTicket = printTicket;
+
+  //////////
+
+  function printTicket() {
+    console.log('tote bäume bewalzen mit: ', vm.formData);
+  }
 };
